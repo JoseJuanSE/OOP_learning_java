408786dbf774a0ce36131a293b6bb7207504226d
JoseJuanSE <jojusuarez46@gmail.com>
2020-12-14 09:32:04 -0600

    some indexes changes and gitignore added

--- Second Partial/Agregacion de clases/Bankito/Cliente.java
+++ Second Partial/Agregacion de clases/Bankito/Cliente.java
@@ -1,39 +1,37 @@
 public class Cliente {
         // Atributo con acceso privado
-        private String nombre;
-        private Cuenta cuenta;
-        private String numCta;
-             
-         // Constructor: inicializa objetos de la clase Cliente
-         public Cliente(String nombre, String numCta){
-                 this.nombre = nombre;
-                 this.numCta = numCta;
-          }
-         //Métodos
-         // obtenerNombre: devuelve el  nombre
-         public String  obtenerNombre( ){
-               return nombre;
-          }  
-          //Métodos
-         // obtenerCuenta: devuelve el objeto de la clase Cuenta
-         public Cuenta obtenerCuenta( ){
-               return cuenta;
-          }  
+      private String nombre;
+      private Cuenta cuenta;
+      private String numCta;
+            
+      // Constructor: inicializa objetos de la clase Cliente
+      public Cliente(String nombre, String numCta){
+            this.nombre = nombre;
+            this.numCta = numCta;
+      }
+      //MÃ©todos
+      // obtenerNombre: devuelve el  nombre
+      public String  obtenerNombre( ){
+            return nombre;
+      }  
+      //MÃ©todos
+      // obtenerCuenta: devuelve el objeto de la clase Cuenta
+      public Cuenta obtenerCuenta( ){
+            return cuenta;
+      }  
 
-        // establecerCuenta: establece el objeto de la clase Cuenta
-         public void establecerCuenta(Cuenta cta ){
-                cuenta = cta;
-          }  
-        // obtenerNombre: devuelve el  numero de cuenta
-         public String  obtenerNumCta( ){
-               return numCta;
-          }
-          
-          public double consultarCuenta(){
-	       		double monto;
-	       		
-	       		monto = cuenta.consultar();
-	       		return monto;    
-          }  
+      // establecerCuenta: establece el objeto de la clase Cuenta
+      public void establecerCuenta(Cuenta cta ){
+            cuenta = cta;
+      }  
+      // obtenerNombre: devuelve el  numero de cuenta
+      public String  obtenerNumCta( ){
+            return numCta;
+      }
+      public double consultarCuenta(){
+            double monto;
+            monto = cuenta.consultar();
+            return monto;    
+      }  
 
 } // Fin de la clase Cuenta
